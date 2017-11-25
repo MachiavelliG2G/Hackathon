f6bf5b5343bfef7846daf74641fda3c78bfed7b3
Xandana <you@example.com>
2017-11-25 20:20:18 +0100

    test

--- frontend/src/routes/Counter/components/Counter.js
+++ frontend/src/routes/Counter/components/Counter.js
@@ -21,9 +21,18 @@ export default class Counter extends React.Component {
   render () {
     this.participants = this.props.counter.participants.map((details, i) => {
       return(
-        <div></div>
+        <div key={i} style={{'textAlign': 'left', 'borderLeft': '6px solid #2196F3 !important', 'backgroundColor': '#ddffff', 'marginBottom': '4px'}}>
+          <h3>Name: {details.name}</h3>
+          <p>HP: {details.hp}/{details.maxhp}</p>
+          <i style={{width: '100%', 'textAlign': 'right'}}>State: {details.state}</i>
+        </div>
       );
-    })
+    });
+    this.logs = this.props.counter.logs.map((details, i) => {
+      return(
+        <p>{details}</p>
+      )
+    });
     return(
       <div style={{ margin: '0 auto' }} >
       <h2>Boss HP: {this.props.counter.boss.hp}/{this.props.counter.boss.maxhp} </h2>
@@ -31,6 +40,11 @@ export default class Counter extends React.Component {
       <button onClick={this.specialattack.bind(this)} className='btnAttack'>On this Button you can perform your special attack</button>
       <div style={{ margin: '0 auto' }}>
         <Link to='/'><button id='homebtn' height='30px' width='50px' className='btnround'>Leave Battle :P</button></Link> 
+      </div>
+      {this.participants}
+      <div style={{'textAlign': 'left', 'borderLeft': '6px solid #2196F3 !important', 'backgroundColor': '#ddffff', 'marginBottom': '4px'}}>
+      <h2>Logs</h2>
+      {this.logs}
       </div>
     </div>
     );
