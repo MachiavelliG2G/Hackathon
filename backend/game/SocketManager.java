5a38710bd9ff20fdcb2e566decb458951e3082ef
ExMachina2 <valentin.kroner@gmail.com>
2017-11-25 13:52:39 +0100

    update

--- backend/game/SocketManager.java
+++ backend/game/SocketManager.java
@@ -1,5 +1,19 @@
 package game;
 
+import data.*;
+
 public class SocketManager {
 
+	
+	public void postFightUpdate(Fight f) {
+		
+	}
+	
+	public void postCharacterUpdate(GameCharacter c) {
+		
+	}
+	
+	
+	public void receive
+	
 }
