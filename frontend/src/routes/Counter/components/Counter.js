2fb5071fe9bf3ade718c1622bb832f50768dafdc
Mathew <khayaanimations@gmail.com>
2017-11-25 16:58:59 +0100

    update

--- frontend/src/routes/Counter/components/Counter.js
+++ frontend/src/routes/Counter/components/Counter.js
@@ -3,28 +3,27 @@ import PropTypes from 'prop-types'
 import './Counter.scss'
 import { Link } from 'react-router'
 
-export const Counter = ({ counter, increment, doubleAsync }) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>Your HP: {counter}</h2>
-    <h2>Boss HP: ? </h2>
-    <button className='btnAttack'>On this Button you can perform your standard attack</button>
-    <button className='btnAttack'>On this Button you can perform your special attack</button>
-    <div style={{ margin: '0 auto' }}>
-      <Link to='/'><button id='homebtn' height='30px' width='50px' className='btnround'>Leave Battle :P</button></Link> 
+export default class Counter extends React.Component {
+  static propTypes = {
+    counter: PropTypes.object.isRequired,
+    increment: PropTypes.func.isRequired,
+    doubleAsync: PropTypes.func.isRequired,
+  }
+  render () {
+    this.participants = this.props.counter.participants.map((details, i) => {
+      return(
+        <div></div>
+      );
+    })
+    return(
+      <div style={{ margin: '0 auto' }} >
+      <h2>Boss HP: {this.props.counter.boss.hp}/{this.props.counter.boss.maxhp} </h2>
+      <button className='btnAttack'>On this Button you can perform your standard attack</button>
+      <button className='btnAttack'>On this Button you can perform your special attack</button>
+      <div style={{ margin: '0 auto' }}>
+        <Link to='/'><button id='homebtn' height='30px' width='50px' className='btnround'>Leave Battle :P</button></Link> 
+      </div>
     </div>
-   {/*<button className='btn btn-primary' onClick={increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-secondary' onClick={doubleAsync}>
-      Double (Async)
-</button> */}
-  </div>
-)
-Counter.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increment: PropTypes.func.isRequired,
-  doubleAsync: PropTypes.func.isRequired,
+    );
+  }
 }
-
-export default Counter
--- frontend/src/routes/Counter/modules/counter.js
+++ frontend/src/routes/Counter/modules/counter.js
@@ -1,54 +1,52 @@
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
-export const COUNTER_DOUBLE_ASYNC = 'COUNTER_DOUBLE_ASYNC'
+export const UPDATE_COMBAT = 'UPDATE_COMBAT'
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function increment (value = 1) {
+export function updateCombatState (state) {
   return {
-    type    : COUNTER_INCREMENT,
-    payload : value
-  }
-}
-
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk! */
-
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch({
-          type    : COUNTER_DOUBLE_ASYNC,
-          payload : getState().counter
-        })
-        resolve()
-      }, 200)
-    })
+    type    : UPDATE_COMBAT,
+    payload : state
   }
 }
 
 export const actions = {
-  increment,
-  doubleAsync
+  updateCombatState
 }
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [COUNTER_INCREMENT]    : (state, action) => state + action.payload,
-  [COUNTER_DOUBLE_ASYNC] : (state, action) => state * 2
+  [UPDATE_COMBAT]    : (state, action) => action.payload,
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = 0
+const initialState = {
+  boss: {
+    name: 'Ogre',
+    hp: 500,
+    maxhp: 600
+  },
+  participants: [
+    {
+      name: 'Mathew',
+      hp: 9999,
+      maxhp: 9999
+    },
+    {
+      name: 'Simi',
+      hp: 1,
+      maxhp: 2
+    }
+  ],
+  logs: []
+}
 export default function counterReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
 
